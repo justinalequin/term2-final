a44055f8a03966329197f7867c9aba2c6b066b7e
JUSTIN ALEQUIN <justinalequin@JUSTINs-MacBook-Pro.local>
2021-12-09 12:20:56 -0500

    Working on hooks

---
+++ client/final-frontend/src/components/Hooks/CheckToken.js
@@ -0,0 +1,25 @@
+import jwtDecode from "jwt-decode";
+
+function CheckToken() {
+  function CheckJwtToken() {
+    let jwtToken = window.localStorage.getItem("jwtToken");
+
+    if (jwtToken) {
+      let decodedToken = jwtDecode(jwtToken);
+      console.log(decodedToken);
+      const currentTime = Date.now() / 1000;
+
+      if (decodedToken.exp < currentTime) {
+        window.localStorage.removeItem("jwtToken");
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+  return { CheckJwtToken };
+}
+
+export default CheckToken;
