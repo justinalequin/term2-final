e6b114fcc2cf6f52778cb771912d0028b7c56963
JUSTIN ALEQUIN <justinalequin@JUSTINs-MacBook-Pro.local>
2021-12-14 08:06:31 -0500

    added hooks

--- client/final-frontend/src/App.js
+++ client/final-frontend/src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import jwtDecode from "jwt-decode";
+
 import "./App.css";
 import MealList from "./components/MealList/MealList";
 import Nav from "./components/Nav/Nav";
+import ProtectedHome from "./components/ProtectedHome/ProtectedHome";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Profile from "./components/Profile/Profile";
+import Favorites from "./components/Favorites/Favorites";
+import Signin from "./components/Signin/Signin";
+import Signup from "./components/Signup/Signup";
 
 function App() {
   const [mealData, setMealData] = useState(null);
