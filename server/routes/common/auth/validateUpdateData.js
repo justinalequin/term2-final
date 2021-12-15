6d6afe2b4076a26901a9651cf9c37e0efd63ad1d
JUSTIN ALEQUIN <justinalequin@JUSTINs-MacBook-Pro.local>
2021-12-15 09:13:14 -0500

    middleware

---
+++ server/routes/common/auth/validateUpdateData.js
@@ -0,0 +1,40 @@
+const { isAlpha, isAlphanumeric, isStrongPassword } = require("validator");
+
+function validateUpdateData(req, res, next) {
+  const { firstName, lastName, username, password, confirmPassword } = req.body;
+
+  let errObj = {};
+  if (!isAlpha(firstName)) {
+    errObj.firstName = "First Name cannot have special characters or numbers";
+  }
+
+  if (!isAlpha(lastName)) {
+    errObj.lastName = "Last Name cannot have special characters or numbers";
+  }
+
+  if (!isAlphanumeric(username)) {
+    errObj.username = "Username cannot have special characters";
+  }
+
+  if (!isStrongPassword(password)) {
+    errObj.password =
+      "Your password must contain 1 lowercase, 1 uppercase, 1 number, 1 special character and at least 8 characters long";
+  }
+
+  if (password !== confirmPassword) {
+    errObj.confirmPassword = "Password and confirm password must match";
+  }
+
+  if (Object.keys(errObj).length > 0) {
+    return res.status(500).json({
+      message: "error",
+      error: errObj,
+    });
+  } else {
+    next();
+  }
+}
+
+module.exports = {
+  validateUpdateData,
+};
