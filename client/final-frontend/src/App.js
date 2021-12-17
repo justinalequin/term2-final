c9b4eacab7b92efff8a5b99163baeb193e94320f
JUSTIN ALEQUIN <justinalequin@JUSTINs-MacBook-Pro.local>
2021-12-17 08:15:15 -0500

    cleaning up app js frontend

--- client/final-frontend/src/App.js
+++ client/final-frontend/src/App.js
@@ -16,32 +16,9 @@ import Profile from "./components/Profile/Profile";
 import Favorites from "./components/Favorites/Favorites";
 import Signin from "./components/Signin/Signin";
 import Signup from "./components/Signup/Signup";
+import MealFetch from "./components/MealFetch/MealFetch";
 
 function App() {
-  const [mealData, setMealData] = useState(null);
-  const [calories, setCalories] = useState(2500);
-  const [diet, setDiet] = useState("ketogenic");
-
-  async function getMealPlan() {
-    try {
-      let payload = await axios.get(
-        `https://api.spoonacular.com/mealplanner/generate?apiKey=15723cdbceaf4152bada86c5f317e672&timeFrame=day&targetCalories=${calories}&diet=${diet}`
-      );
-      setMealData(payload);
-      console.log(diet);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  function handleChange(e) {
-    setCalories(e.target.value);
-  }
-
-  function onChangeValue(e) {
-    setDiet(e.target.value);
-  }
-
   return (
     <div className="App">
       <Router>
@@ -53,33 +30,7 @@ function App() {
 
         <hr />
 
-        <div className="top">
-          <br />
-          <h2>Please enter your daily caloric goal</h2>
-          <input
-            style={{ textAlign: "center" }}
-            type="number"
-            placeholder="Default is 2500 cal"
-            onChange={handleChange}
-          />
-          <h2>Please select your type of diet</h2>
-        </div>
-
-        <select className="dietList" name="dietList" onChange={onChangeValue}>
-            <option value="Ketogenic">KETOGENIC</option> 
-          <option value="Vegetarian">VEGETARIAN</option>
-          <option value="Vegan">VEGAN</option>
-          <option value="Paleo">PALEO</option>
-          <option value="Pescetarian">PESCETARIAN</option>
-          <option value="Primal">PRIMAL</option>
-        </select>
-
-        <div className="buttons">
-          <button onClick={getMealPlan}>GET DAILY MEAL PLAN</button>
-        </div>
-
-        <br />
-        <div>{mealData && <MealList mealData={mealData} />}</div>
+        <MealFetch></MealFetch>
       </Router>
     </div>
   );
---
+++ client/final-frontend/src/components/MealFetch/MealFetch.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import Collapsible from "react-collapsible";
+
+function MealFetch() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2500);
+  const [diet, setDiet] = useState("ketogenic");
+
+  async function getMealPlan() {
+    try {
+      let payload = await axios.get(
+        `https://api.spoonacular.com/mealplanner/generate?apiKey=15723cdbceaf4152bada86c5f317e672&timeFrame=day&targetCalories=${calories}&diet=${diet}`
+      );
+      setMealData(payload);
+      console.log(diet);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  function handleChange(e) {
+    setCalories(e.target.value);
+  }
+
+  function onChangeValue(e) {
+    setDiet(e.target.value);
+  }
+
+  return (
+    <div>
+      <div className="top">
+        <br />
+        <h2>Please enter your daily caloric goal</h2>
+        <input
+          style={{ textAlign: "center" }}
+          type="number"
+          placeholder="Default is 2500 cal"
+          onChange={handleChange}
+        />
+        <h2>Please select your type of diet</h2>
+      </div>
+
+      <select className="dietList" name="dietList" onChange={onChangeValue}>
+          <option value="Ketogenic">KETOGENIC</option> 
+        <option value="Vegetarian">VEGETARIAN</option>
+        <option value="Vegan">VEGAN</option>
+        <option value="Paleo">PALEO</option>
+        <option value="Pescetarian">PESCETARIAN</option>
+        <option value="Primal">PRIMAL</option>
+      </select>
+
+      <div className="buttons">
+        <button onClick={getMealPlan}>GET DAILY MEAL PLAN</button>
+      </div>
+
+      <br />
+      <div>{mealData && <MealList mealData={mealData} />}</div>
+    </div>
+  );
+}
+
+export default MealFetch;
